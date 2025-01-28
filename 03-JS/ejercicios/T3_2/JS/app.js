3882bc9935e98f3f35825f97e00accc5aac518db
Alberto <viguerasalbert@gmail.com>
2025-01-28 19:08:07 +0100

    actualización DOM

--- 03-JS/ejercicios/T3_2/JS/app.js
+++ 03-JS/ejercicios/T3_2/JS/app.js
@@ -77,66 +77,118 @@ btnLimpiar2.onclick = () => {
   limpiar(mostrar2, frase);
 }
 
+
+
 //ejercicio 3
-formulari = document.querySelector("#formulariEx3");
-let nom;
-let cognom;
-let edat;
-let DNI;
 
-function recollirDadesFormulari(formulari) {
-  nom = formulari[0].value;
-  cognom = formulari[1].value;
-  edat = formulari[2].value;
-  DNI = formulari[3].value;
+let formulari = document.querySelector("#formulariEx3");
+let btn3 = document.querySelector("#btn_Ej_tres");
+let mostrar3 = document.querySelector("#mostrar3");
+
+function crearUsuario(item) {
+  let nom = item.elements["nom"].value;
+  let cognom = item.elements["cognom"].value;
+  let edat = item.elements["edat"].value;
+  let dni = item.elements["dni"].value;
+  let persona = {
+    nom: nom,
+    cognom: cognom,
+    edat: edat,
+    dni: dni
+  }
+  //dues maneres de mostrar les dades:
+/*   mostrar3.innerHTML = JSON.stringify(persona);//ho mostra tal com es veuria a l'array
+ */  mostrar3.innerHTML = `Nom: ${nom}. Cognom: ${cognom}. Edat:${edat}. DNI:${dni}`;
 }
 
-document.querySelector("#btn_Ej_tres").onclick = () => {
-  alert(typeof (nom)); alert(typeof (cognom)); alert(typeof (edat)); alert(typeof (DNI));
+btn3.onclick = () => {
+  crearUsuario(formulari);
 }
 
-recollirDadesFormulari(formulari);//omplim les variantes cerades a dal amb el valors recollits del formolari
+/* let personas = [
+  {
+    nom: "Alberto", cognom: "Hernandez", edat: 23
+  },
+  {
+    nom: "Rogelio", cognom: "fernandez", edat: 33
 
-function validarNom(item) {//creem les funciones per validar els 4 camps
-  item = item.value;
-  let regEx = new RegExp("^[a-z ,.'-]+$i");
-  let test = regEx.test(item);
-  return test;
+  }
+]
+ */
+
+//ejercicio4
+
+let formulari4 = document.querySelector("#formulariEx4");
+let mostrar4 = document.querySelector("#mostrar4");
+let btn4 = document.querySelector("#btn_Ej_cuatro");
+let nom4 = formulari4.elements["nom4"].value;
+let cognom4 = formulari4.elements["cognom4"].value;
+let edat4 = formulari4.elements["edat4"].value;
+let dni4 = formulari4.elements["dni4"].value;
+
+function recogerDatos(item) {
+  nom4 = item.elements["nom4"].value;
+  cognom4 = item.elements["cognom4"].value;
+  edat4 = item.elements["edat4"].value;
+  dni4 = item.elements["dni4"].value;
 }
-function validarCognom(item) {
-  item = item.value;
-  let regEx = new RegExp("/^[a-z ,.'-]+$/i");
-  let test = regEx.test(item);
-  return test;
+
+//validacions
+/* function validarText(item) {
+  let validacio;
+  if (item.length < 3 || item.length > 20 || item.match(/\d/) || item === "") {
+    validacio = false;
+  }
+  return validacio;
 }
 function validarEdat(item) {
-  item = item.value;
-  let regEx = new RegExp("^(1[89]|[2-9]\\d{1,2}|[1-9]\\d{3,})$");
-  let test = regEx.test(item);
-  return test;
+  let validacio = true;
+  if (item < 17 || isNaN(item)) {
+    validacio = false;
+  }
+  return validacio;
 }
 function validarDNI(item) {
-  item = item.value;
-  let regEx = new RegExp("^\d{8}[A-Za-z]$");
-  let test = regEx.test(item);
-  return test;
-}
-
-
-/* if (!validarNom(nom)) {
-  document.querySelector("#mostrar3").innerHTML = "Error a l'entrada del nom";
-}
-else if (!validarCognom(cognom)) {
-  document.querySelector("#mostrar3").innerHTML = "Error a l'entrada del cognom";
-}
-else if (!validarEdat(edat)) {
-  document.querySelector("#mostrar3").innerHTML = "Error a l'entrada de l'edat";
-}
-
-else if (!validarEdat(DNI)) {
-  document.querySelector("#mostrar3").innerHTML = "Error a l'entrada deL DNI";
-}
-
-else {
-
+  let exreg = /^[A-Za-z]{7}\d$/;
+  let validacio = true;
+  if (item.length !== 8) {
+    validacio = false;
+  } else if (!exreg.test(item)) {
+    validacio = false;
+  }
+  return validacio;
 } */
+let personas = [];
+
+/* function validació() {
+  if (!validarText(nom4)) {
+    document.querySelector("#errorNom").innerHTML = `<p class="alert alert-danger" role="alert> Error introduïnt el nom </p>`
+
+  }
+  else if (!validarText(cognom4)) {
+    document.querySelector("#errorCognom").innerHTML = `<p class="alert alert-danger" role="alert> Error introduïnt el cognom </p>`
+  }
+  else if (!validarEdat(edat4)) {
+    document.querySelector("#errorEdat").innerHTML = `<p class="alert alert-danger" role="alert> Error introduïnt l'edat</p>`
+  }
+  else if (!validarDNI(dni4)) {
+    document.querySelector("#errorDni").innerHTML = `<p class="alert alert-danger" role="alert> Error introduïnt el DNI</p>`
+  } else {
+    let persona = {
+      "Nom": nom4, "Cognom": cognom4, "Edat": edat4, "DNI": dni4
+    }
+    personas.push(persona);
+    mostrar4.innerHTML = JSON.stringify(personas);
+  }
+} */
+let persona = {
+  "Nom": nom4, "Cognom": cognom4, "Edat": edat4, "DNI": dni4
+}
+personas.push(persona);
+mostrar4.innerHTML = JSON.stringify(personas);
+
+
+btn4.onclick = () => {
+  recogerDatos(formulari4);
+  validació();
+}
