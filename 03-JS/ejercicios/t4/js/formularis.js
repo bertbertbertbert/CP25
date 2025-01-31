048ccdad664600527c310bdfdca45f70ec086016
AlbertoCasa <viguerasalbert@gmail.com>
2025-01-31 10:26:01 +0100

    he comentado la validacion de edad, va a necesitar mirarse bien

--- 03-JS/ejercicios/t4/js/formularis.js
+++ 03-JS/ejercicios/t4/js/formularis.js
@@ -6,31 +6,36 @@ const dniPattern = /^[XYZ]?\d{7,8}[A-Z]{1}$/;
 const emailPattern = /^[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,5}$/;
 
 //funcion, comporbar todo correcto
-btnRegistro.addEventListener("submit", (event) => {
+btnRegistro.addEventListener("click" /* "submit" */, (event) => {
+  alert("hola");
   const vNombre = nombre();
-  const vEdad = edad();
-  const vTel = telefono();
+/*   const vEdad = edad();
+ */  const vTel = telefono();
   const vEmail = email();
   const vDni = dni();
   const vConsent = consent();
-
-  if (!vNombre || !vEdad || !vTel || !vEmail || !vDni || !vConsent) {
+  if (!vNombre /* || !vEdad  */|| !vTel || !vEmail || !vDni || !vConsent) {
     event.preventDefault();
     mostrar.innerHTML = "Datos introducidos incorrectamente"
     mostrar.style.color = "red";
+  }else{
+    mostrar.innerHTML = "Datos introducidos incorrectamente"
+    mostrar.style.color = "green";
+    
   }
 });
 
 const nombre = () => {
   let nombre = form.nombre.value;
   if (nombre = "" || nombre.charAt(0).match(numeroPattern) || nombre.length <= 3 || nombre.length >= 20) {
+    console.log("fallo en nombre");
     return false;
   } else {
     return true;
   }
 }
 
-const edad = () => {
+/* const edad = () => {
   let dia = form.dia.value;
   let mes = form.mes.value;
   let anio = form.anio.value;
@@ -50,12 +55,13 @@ const edad = () => {
       true
     }
   }
-}
+} */
 
 const telefono = () => {
   let telefono = form.telefono.value;
   telefono = parseInt(telefono);
   if (telefono === "" || isNaN(telefono) || !telefono.length === 9) {
+    console.log("fallo en telefono");
     return false;
   } else {
     return true;
@@ -66,6 +72,7 @@ const email = () => {
   let email = form.email.value;
   email = email.toLowerCase();
   if (email === "" || !email.match(emailPattern)) {
+    console.log("fallo en email");
     return false
   } else {
     return true;
@@ -86,14 +93,17 @@ const dni = () => {
     unaLetra = dni.substr(dni.length - 1, 1);
     numero = numero % 23;
     letra = letra.substring(numero, numero + 1);
-    if (letra !== unaLetra) return false;
-    return true;
+    if (letra !== unaLetra){console.log("fallo en dni letra");
+    return false;}else{
+    return true;}
   } else {
+    console.log("fallo en dni numeros")
     return false;
   }
 }; const consent = () => {
   let checked = form.condiciones.value;
-  if (!checked) {
+  if (checked===false) {
+    console.log("fallo en dni checked")
     return false;
   } else {
     return true;
