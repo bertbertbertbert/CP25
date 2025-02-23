5913b27b307ae437e1559dac6bb3c8e5209ccf78
AlbertoCasa <viguerasalbert@gmail.com>
2025-02-23 23:53:54 +0100

    23-2-25

--- 03-JS/17-APIRest_Node/js/app.js
+++ 03-JS/17-APIRest_Node/js/app.js
@@ -1,11 +1,24 @@
-
 const d = document;
 const cargarUsuarios = d.querySelector("#usuarios");
+const btnBuscar = d.querySelector("#buscarBtn");
 const mostrar = d.querySelector("#contenido");
 const fragmento = d.createDocumentFragment();
 const formTemplate = d.querySelector("#form-template").content;
 const template = d.querySelector("#crud-template").content;
-const tabla = formTemplate.querySelector(".crud-table tbody");
+const id=d.querySelector("#id");
+/* const formTemplate = d.querySelector(".crud-table tbody"); */
+
+const encontrarEmpleado=((empleados)=>{
+  form=d.querySelector("#crud-form");
+  empleados.forEach(empleado =>{
+    if(id.value==empleado.id){
+      form.nombre.value=empleado.nombre;
+      form.empresa.value=empleado.empresa;
+      form.puesto.value=empleado.puesto;
+      d.querySelector("#error").innerHTML="";
+    }
+  })
+})
 
 const mostrarEmpleados = ((empleados) => {
   mostrar.innerHTML = "";
@@ -19,19 +32,20 @@ const mostrarEmpleados = ((empleados) => {
     clone.querySelector(".delete");
     fragmento.appendChild(clone);
   });
-  tabla.appendChild(fragmento);
+  formTemplate.appendChild(fragmento);
   mostrar.appendChild(formTemplate);
 })
 
 const obtListaEmpleados = () => {
-  fetch("http://localhost:3000/users")
+  fetch("/03-JS/14_Api_fetch/server/db.json")
     .then((response) => {
       if (!response.ok) throw new Error("error en la solicitud");
       return response.json();
     })
     .then((data) => {
       console.log(data);
-      mostrarEmpleados(data);
+      mostrarEmpleados(data.users);
+      encontrarEmpleado(data.users);
     })
     .catch((error) => {
       console.log("error a cargar los empleados", error);
@@ -41,4 +55,6 @@ const obtListaEmpleados = () => {
 cargarUsuarios.onclick = () => {
   obtListaEmpleados();
 }
-
+btnBuscar.onclick = ()=>{
+  obtListaEmpleados();
+}
